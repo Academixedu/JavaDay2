d86aedbf9765224a4758c0076670862b75e3a051
gonchigars <gonchigars@gmail.com>
2024-05-27 11:30:17 +0530

    added comment

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,12 @@
 package com.example;
 
+/**
+ * Main
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        
-        
     }
+// adding comment here
+    
 }
