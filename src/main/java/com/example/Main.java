2dfa98fa74f4e91057836e29def9ad37e9f0cd53
gonchigars <gonchigars@gmail.com>
2024-05-27 17:44:43 +0530

    Practice format..

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,18 @@
 package com.example;
 
 /**
- * Main
+ * Main class that contains the entry point of the application.
  */
 public class Main {
+
+    /**
+     * The main method is the entry point of the application.
+     * It prints "Hello world!" to the console.
+     * 
+     * @param args Command-line arguments passed to the program (not used).
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Print "Hello world!" to the console
+        
     }
-// adding comment here
-    
 }
